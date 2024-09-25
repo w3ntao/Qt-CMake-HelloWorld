2d24fcadf17bb48aa913ff43dafd35b76fb21769
Wentao Liu <wentao.liu.zero@outlook.com>
2024-09-25 16:30:23 +0800

    rewriting

--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
+#define PY_SSIZE_T_CLEAN
+
+#include <Python.h>
+
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    std::cout << "main window created\n" << std::flush;
+
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        exit(1);
+    }
+
+    Py_SetProgramName(program);  /* optional but recommended */
+
+    Py_Initialize();
+
+    std::string filename = "../qdiag.py";
+    PyObject *obj = Py_BuildValue("s", filename.c_str());
+    FILE *fp = _Py_fopen_obj(obj, "r+");
+    if (fp == NULL) {
+        std::cerr << "file `" << filename << "` not found\n\n";
+        exit(1);
+    }
+
+    PyRun_SimpleFile(fp, filename.c_str());
+    fclose(fp);
+
+    printf("python script `%s` executed\n", filename.c_str());
+
+    if (Py_FinalizeEx() < 0) {
+        exit(120);
+    }
+
+    PyMem_RawFree(program);
+
 
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,33 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "ui_mainwindow.h"
+
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = 0) :
+            QMainWindow(parent),
+            ui(new Ui::MainWindow) {
+        ui->setupUi(this);
+    }
+
+    ~MainWindow() {
+        delete ui;
+    }
 
 private slots:
-    void on_actionExit_triggered();
+
+    void on_actionExit_triggered() {
+        this->close();
+
+    };
 
 private:
     Ui::MainWindow *ui;
